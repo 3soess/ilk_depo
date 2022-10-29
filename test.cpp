6d3f2bde6c1ca35376d94f16d0ab8532e49d0b83
Eslem Arik <eslemarik20@outlook.com>
2022-10-29 22:07:45 +0300

    Not harf hesabı

--- test.cpp
+++ test.cpp
@@ -1,4 +1,23 @@
-int main() {
-
+int main()
+{
+    int a;
+    cout << "Notunuzu ekrana giriniz. " << endl;
+    cin >> a;
+    if (a < 50)
+    {
+        cout << "Aldiginiz not: F" << endl;
+    }
+    else if (a < 70)
+    {
+        cout << "Aldiginiz not: C" << endl;
+    }
+    else if (a < 90)
+    {
+        cout << "Aldiginiz not: B" << endl;
+    }
+    else
+    {
+        cout << "Aldiginiz not: A" << endl;
+    }
     return 0;
 }
